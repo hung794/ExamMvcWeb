12e6df863c14a188df86b994b977a5ba4e68c04d
Quoc Hung <Quoc Hung@DESKTOP-NHAT11T>
2022-05-10 22:19:42 +0700

    Add project files.

---
+++ ExamMvcWeb/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExamMvcWeb.EmployeeService;
+
+namespace ExamMvcWeb.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        Service1Client service = new Service1Client();
+
+        [HttpGet]
+        public ActionResult Index(string keyword)
+        {
+            ViewBag.keyword = keyword;
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                var employees = service.SearchEmployee(keyword);
+                return View(employees);
+            }
+            else
+            {
+                return View(service.FindAll());
+            }
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(Employee employee)
+        {
+            bool result = service.AddEmployee(employee);
+            if (result)
+            {
+                return Redirect("/Employee/Index");
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+}
---
+++ ExamMvcWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExamMvcWeb.MvcApplication" Language="C#" %>
